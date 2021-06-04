caf6d42363e80826cf35be9be5ae373d326b0acf
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2021-06-04 18:29:21 +0300

    Fixed UserCard entity

--- src/main/java/ru/realdating/project/model/UserCard.java
+++ src/main/java/ru/realdating/project/model/UserCard.java
@@ -1,22 +1,44 @@
 package ru.realdating.project.model;
 
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
+import java.sql.Blob;
 
 @Entity
 public class UserCard {
+
     // это карточка-анкета пользователя
     @Id
     @GeneratedValue
     private int id;
 
+    // загрузка картинки для аватар (правда не умею с Blob работать)
+    @Column
+    private Blob avatar;
+
+    @Column (columnDefinition="TEXT")
+    private String aboutMe;
+
+    @Column (columnDefinition="TEXT")
+    private String interests;
+
+    @Column (length = 10, nullable = false)
+    private int age;
+
+    @Column (length = 20, nullable = false)
+    private String gender;
+
+    @Column (length = 50)
+    private String status;
+
     // лайк, которые пользователи ставят карточке
     @Column
     @ColumnDefault(value = "0")
-    private int like;
+    private int likeUserCard;
 
     @OneToOne
-    private User user_id;
+    private User userId;
 
 }
