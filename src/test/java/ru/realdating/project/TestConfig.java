cc1133852a59e5311b80fe8691fb952d988298ae
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2021-06-18 16:50:54 +0300

    Fixed Testconfig, Tests is working

--- src/test/java/ru/realdating/project/TestConfig.java
+++ src/test/java/ru/realdating/project/TestConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import ru.realdating.project.config.AppConfig;
 import ru.realdating.project.config.ProdJpaConfig;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -14,10 +16,15 @@ import javax.persistence.Persistence;
         basePackages = "ru.realdating.project",
         excludeFilters = @ComponentScan.Filter(
                 type = FilterType.ASSIGNABLE_TYPE,
-                classes = {ProdJpaConfig.class, App.class}
+                classes = {ProdJpaConfig.class, App.class, AppConfig.class}
         )
 )
 public class TestConfig {
+
+    @Bean
+    public EntityManager manager (EntityManagerFactory factory) {
+        return factory.createEntityManager();
+    }
 
     @Bean
     public EntityManagerFactory factory() {
