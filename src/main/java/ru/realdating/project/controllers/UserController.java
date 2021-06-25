62028dc7be2985f97917e17d104f13dd5e2ed8c5
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2021-06-25 17:40:03 +0300

    Add dependency Spring validation + Validate Register form and Register controller

--- src/main/java/ru/realdating/project/controllers/UserController.java
+++ src/main/java/ru/realdating/project/controllers/UserController.java
@@ -2,12 +2,18 @@ package ru.realdating.project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import ru.realdating.project.dao.UserCardDao;
 import ru.realdating.project.dao.UserDao;
 import ru.realdating.project.model.User;
 import ru.realdating.project.model.UserCard;
+import ru.realdating.project.service.RegistrationForm;
 import ru.realdating.project.service.UserSession;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(path = "/user")
@@ -21,23 +27,52 @@ public class UserController {
     private UserCardDao userCardDao;
 
     @GetMapping("/register")
-    public String userRegister() {
+    public String userRegister(@ModelAttribute("registrationForm") RegistrationForm registrationForm) {
+
         return "/user/user_register";
     }
 
     @PostMapping("/register")
     public String handleRegister(
-            @RequestParam String login,
-            @RequestParam String password
-    ) {
-        User userForCheck = userDao.findUserByLogin(login);
-        if (!(login.isEmpty() || password.isEmpty()) && userForCheck == null) {
-            User user = userDao.createUser(login, password);
-            userCardDao.createUserCard(user);
-            return "redirect:/";
+            @ModelAttribute("registrationForm")
+            @Valid RegistrationForm registrationForm,
+            BindingResult bindingResult
+            ) {
+
+        if (!registrationForm.getPassword().equals(registrationForm.getPasswordConfirmation())) {
+            bindingResult.addError(new FieldError(
+                    "registrationForm",
+                    "passwordConfirmation",
+                    "Password and confirmation password should match"));
         }
-        return "redirect:/user/register";
+
+        if (bindingResult.hasErrors()) {
+            return "/user/user_register";
+        }
+
+        User userForCheck = userDao.findUserByLogin(registrationForm.getLogin());
+        if (userForCheck == null) {
+            userCardDao.createUserCard(userDao.createUser(registrationForm.getLogin(), registrationForm.getPassword()));
+            return "redirect:/";
+        } else {
+            return "/user/user_register";
+        }
     }
+
+// Old Post without @Valid
+//    @PostMapping("/register")
+//    public String handleRegister(
+//            @RequestParam String login,
+//            @RequestParam String password
+//    ) {
+//        User userForCheck = userDao.findUserByLogin(login);
+//        if (!(login.isEmpty() || password.isEmpty()) && userForCheck == null) {
+//            User user = userDao.createUser(login, password);
+//            userCardDao.createUserCard(user);
+//            return "redirect:/";
+//        }
+//        return "redirect:/user/register";
+//    }
 
     @GetMapping("/log-in")
     public String logIn() {
@@ -58,6 +93,11 @@ public class UserController {
         }
         userSession.clearSession();
         return "redirect:/user/log-in";
+    }
+
+    @ModelAttribute("registrationForm")
+    public RegistrationForm createDefault() {
+        return new RegistrationForm();
     }
 
 }
--- src/main/java/ru/realdating/project/service/RegistrationForm.java
+++ src/main/java/ru/realdating/project/service/RegistrationForm.java
@@ -1,9 +1,31 @@
 package ru.realdating.project.service;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class RegistrationForm {
 
+    @NotBlank
+    @NotEmpty
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "[a-zA-Z0-9]{4,10}",
+            message = "Login must consist of letters, numbers and from 4 to 10 chars")
     private String login;
+
+    @NotBlank
+    @NotEmpty
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "[a-zA-Z0-9]{4,10}",
+            message = "Password must consist of letters, numbers and from 4 to 10 chars")
     private String password;
+
+    @NotBlank
+    @NotEmpty
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "[a-zA-Z0-9]{4,10}",
+            message = "Password must consist of letters, numbers and from 4 to 10 chars")
     private String passwordConfirmation;
 
     public String getLogin() {
